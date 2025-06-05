20614837023b01307af176f9c20a33f21caf87c5
Shivani Bhat <112406446+shivanibhat24@users.noreply.github.com>
2025-06-05 23:39:00 +0530

    Create main.h

---
+++ STM32_Self Healing Flash Engine/main.h
@@ -0,0 +1,40 @@
+/* ========== HEADER FILE CONTENT (main.h) ========== */
+/*
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_ll_flash.h"
+#include "stm32f4xx_ll_rcc.h"
+#include "stm32f4xx_ll_bus.h"
+#include "stm32f4xx_ll_system.h"
+#include "stm32f4xx_ll_exti.h"
+#include "stm32f4xx_ll_cortex.h"
+#include "stm32f4xx_ll_utils.h"
+#include "stm32f4xx_ll_pwr.h"
+#include "stm32f4xx_ll_dma.h"
+#include "stm32f4xx_ll_usart.h"
+#include "stm32f4xx_ll_gpio.h"
+
+void Error_Handler(void);
+
+#define LED_PIN                          GPIO_PIN_5
+#define LED_GPIO_Port                    GPIOA
+#define USER_BUTTON_PIN                  GPIO_PIN_13
+#define USER_BUTTON_GPIO_Port            GPIOC
+#define USART_TX_Pin                     GPIO_PIN_2
+#define USART_TX_GPIO_Port               GPIOA
+#define USART_RX_Pin                     GPIO_PIN_3
+#define USART_RX_GPIO_Port               GPIOA
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+*/
+/* ========== END OF HEADER FILE CONTENT ========== */
